df6c08dae756da9439a01615d01e93a2dc179669
dejangvero <dejangverobusiness@gmail.com>
2023-03-01 22:41:39 +0100

    Adding a GET a single user test and using docString

--- features/herokuapp-SupertestExample.feature
+++ features/herokuapp-SupertestExample.feature
@@ -1,7 +1,26 @@
-@smoke
 Feature: Example APi testing using supertest and https://reqres.in/
 
-        Scenario Outline: As a user, I want to get the list of users and assert thati got a response body that contains users
+    @smoke
+    Scenario: As a user, I want to get the list of users and assert that i got a response body that contains users
+        Given I send a GET request to REGRES GET USERS Endpoint
+        Then I receive a list of users
 
-            Given I send a GET request to REGRES GET USERS Endpoint
-             Then I receive a list of users
+    @smoke
+    Scenario: As i user, i want to get a single user by ID
+        Given I send a GET user request with ID: 2
+        Then I want to receive the following response in the body:
+            """
+            {
+                "data": {
+                    "id": 2,
+                    "email": "janet.weaver@reqres.in",
+                    "first_name": "Janet",
+                    "last_name": "Weaver",
+                    "avatar": "https://reqres.in/img/faces/2-image.jpg"
+                },
+                "support": {
+                    "url": "https://reqres.in/#support-heading",
+                    "text": "To keep ReqRes free, contributions towards server costs are appreciated!"
+                }
+            }
+            """
--- features/step-definitions/Herokuapp-SupertestExampleSteps.ts
+++ features/step-definitions/Herokuapp-SupertestExampleSteps.ts
@@ -3,18 +3,29 @@ import { JsonArray } from "@wdio/types";
 import supertest from 'supertest';
 
 const request = supertest('https://reqres.in/api');
-var apiStatusCode:any;
-var apiResponseBody:any;
-var apiResponseBodyDataArray:JsonArray;
+var response:any;
+var apiStatusCode: any;
+var apiResponseBody: any;
+var apiResponseBodyDataArray: JsonArray;
 
-Given('I send a GET request to REGRES GET USERS Endpoint', async () => {    
-      const response = await request.get('/users?page=2');
-      apiStatusCode = response.statusCode;
-      apiResponseBody = response.body;
-      apiResponseBodyDataArray = apiResponseBody.data;    
+Given('I send a GET request to REGRES GET USERS Endpoint', async () => {
+    response = await request.get('/users?page=2');
+    apiStatusCode = response.statusCode;
+    apiResponseBody = response.body;
+    apiResponseBodyDataArray = apiResponseBody.data;
 });
 
-Then('I receive a list of users', async () => { 
+Then('I receive a list of users', async () => {
     expect(apiStatusCode).toStrictEqual(200);
     expect(apiResponseBodyDataArray.length).toStrictEqual(6);
+});
+
+Given('I send a GET user request with ID: {int}', async (id: number) => {
+    response = await request.get(`/users/${id}`);      
+});
+
+Then('I want to receive the following response in the body:', async (docString:string) => {
+    expect(response.statusCode).toStrictEqual(200);
+    const jsonBody:JSON = response.body;
+    expect(jsonBody).toStrictEqual(JSON.parse(docString));
 });
