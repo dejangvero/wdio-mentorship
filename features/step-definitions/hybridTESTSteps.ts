f414c57fc8637cb35a4b3dab533437f7e702e5c0
Dejan Gvero <gvero.d@buchi.com>
2023-04-07 14:05:12 +0200

    - Final hybrid test

--- features/step-definitions/hybridTESTSteps.ts
+++ features/step-definitions/hybridTESTSteps.ts
@@ -1,33 +1,7 @@
 import { Given, When, Then } from '@wdio/cucumber-framework';
 import supertest from 'supertest';
-import express from 'express';
-import bodyParser from 'body-parser';
-
-// const app = express();
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({extended: true}));
-
 
 const request = supertest('https://app.brmodeloweb.com');
-
-
-// Given('I login to automation exercise via API',  async () => {
-//     response = await request.post('/my-account/')
-//     .type('form')    
-//     .field('username', 'using123@yopmail.com')
-//     .field('password', 'Using123*+')    
-//     .expect(200)  
-// });
-
-// When('I navigate to automation exercise page',  async () => {
-//     await browser.url(`https://practice.automationtesting.in/my-account/`);
-// });
-
-
-// Then('I will see the Logout button',  async () => {
-//     const logoutButton = await $$("a[href='https://practice.automationtesting.in/my-account/customer-logout/']")[0];
-//     expect(logoutButton.getText()).toStrictEqual('Logout');
-// });
 
 Given('I login to BR Modelo Web via API',  async () => {    
     const payload = {username: 'dXNpbmcxMjNAeW9wbWFpbC5jb20', password: 'VXNpbmcxMjM' };
